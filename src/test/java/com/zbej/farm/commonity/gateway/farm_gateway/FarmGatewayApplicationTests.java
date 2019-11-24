081092fe850be7984e2c3dc39f3fd46bf4061473
Morton <578539614@qq.com>
2019-11-24 09:40:12 +0800

    gatewayV1

---
+++ src/test/java/com/zbej/farm/commonity/gateway/farm_gateway/FarmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbej.farm.commonity.gateway.farm_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
